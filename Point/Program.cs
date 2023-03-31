da269f6759ed7e6c8e0467245974741bbd641a3e
ben478 <benleon@trentu.ca>
2023-03-30 22:20:43 -0400

    new print method just dropped

--- Point/Program.cs
+++ Point/Program.cs
@@ -1,312 +1,368 @@
+using System.Linq;
 // Point Class
-    // Represents a point in the n-th dimension
-    public class Point
+// Represents a point in the n-th dimension
+public class Point
+{
+    private float[] coord { get; set; } // Coordinate storage 
+
+    // Constructor
+    // Creates a zero point in the specified dimension
+    public Point(int dim)
     {
-        private float[] coord { get; set; } // Coordinate storage 
+        coord = new float[dim]; // Create array to store 
+    }
 
-        // Constructor
-        // Creates a zero point in the specified dimension
-        public Point(int dim)
+    // getDim
+    // Returns the dimension the point is in
+    public int getDim()
+    {
+        return coord.Length;
+    }
+
+    // Get
+    // Returns the i-th dimension value in a coordinate
+    public float Get(int i)
+    {
+        return coord[i];
+    }
+
+    // Set
+    // Sets the i-th dimension value in a cordinate
+    public void Set(int i, float value)
+    {
+        coord[i] = value;
+    }
+
+    // distanceTo
+    // Returns the Euclidean distance between two points in the nth-dimensions
+    public float distanceTo(Point other)
+    {
+        double distance = 0;
+
+        for (int i = 0; i < getDim(); i++) // For each dimension
         {
-            coord = new float[dim]; // Create array to store 
+            distance += Math.Pow(other.coord[i] - this.coord[i], 2);
         }
+        return (float)Math.Sqrt(distance);
+    }
 
-        // getDim
-        // Returns the dimension the point is in
-        public int getDim()
+    // Equals Override
+    // Returns if a point in k-th dimensional space 
+    public override bool Equals(Object? obj)
+    {
+        if (obj != null) // Null check
         {
-            return coord.Length;
-        }
-
-        // Get
-        // Returns the i-th dimension value in a coordinate
-        public float Get(int i)
-        {
-            return coord[i];
-        }
-
-        // Set
-        // Sets the i-th dimension value in a cordinate
-        public void Set(int i, float value)
-        {
-            coord[i] = value;
-        }
-
-        // distanceTo
-        // Returns the Euclidean distance between two points in the nth-dimensions
-        public float distanceTo(Point other)
-        {
-            double distance = 0;
-
+            Point p = (Point)obj; // Downcast 
             for (int i = 0; i < getDim(); i++) // For each dimension
             {
-                distance += Math.Pow(other.coord[i] - this.coord[i], 2);
+                if (this.coord[i] != p.coord[i]) return false; // Compare values
             }
-            return (float)Math.Sqrt(distance);
+            return true;
         }
-
-        // Equals Override
-        // Returns if a point in k-th dimensional space 
-        public override bool Equals(Object? obj)
-        {
-            if (obj != null) // Null check
-            {
-                Point p = (Point)obj; // Downcast 
-                for (int i = 0; i < getDim(); i++) // For each dimension
-                {
-                    if (this.coord[i] != p.coord[i]) return false; // Compare values
-                }
-                return true;
-            }
-            return false; // Null
-        }
-
-        // ToString Override
-        // Converts the point to a string
-        public override string ToString()
-        {
-            string pointString = "( "; // Empty string
-
-            for (int i = 0; i < getDim(); i++) // For each dimension, concatenate the dimension value to the return string
-            {
-                pointString += coord[i]+ " ";
-            }
-            pointString += ")";
-            return pointString;
-        }
-
-
+        return false; // Null
     }
 
-
-    class KDNode
-    { // node in a kd-tree
-        public Point point; // splitting point
-        public int cutDim; // cutting dimension
-        public KDNode left; // children
-        public KDNode right;
-
-
-        // constructor
-        public KDNode(Point point, int cutDim)
-        { 
-            this.point = point;
-            this.cutDim = cutDim;
-            left = right = null;
-        }
-
-        // is x in left subtree?
-        public bool inLeftSubtree(Point x)
-        { 
-            return x.Get(cutDim) < point.Get(cutDim);   
-        }
-
-        //Override ToString()
-        public override string ToString()
-        {
-            return "(" +" "+ point.ToString() + ")" + cutDim.ToString();
-        }
-    }
-
-    class KDTree
+    // ToString Override
+    // Converts the point to a string
+    public override string ToString()
     {
-        public KDNode root;
-        float maxVal;
+        string pointString = "("; // Empty string
 
-        // default constructor
-        //root is null and max float value of a point is 100
-        public KDTree()
+        for (int i = 0; i < getDim(); i++) // For each dimension, concatenate the dimension value to the return string
         {
-            this.root = null;
-            maxVal = 100.0f;
+            pointString += coord[i]+ " ";
         }
+        pointString = pointString.Substring(0, pointString.Length - 1) + ")";
+        return pointString;
+    }
 
-        //overloaded constructor 1
-        //custom root and custom max point value
-        public KDTree(KDNode root, int max)
+
+}
+
+
+class KDNode
+{ // node in a kd-tree
+    public Point point; // splitting point
+    public int cutDim; // cutting dimension
+    public KDNode left; // children
+    public KDNode right;
+
+
+    // constructor
+    public KDNode(Point point, int cutDim)
+    { 
+        this.point = point;
+        this.cutDim = cutDim;
+        left = right = null;
+    }
+
+    // is x in left subtree?
+    public bool inLeftSubtree(Point x)
+    { 
+        return x.Get(cutDim) < point.Get(cutDim);   
+    }
+
+    //Override ToString()
+    public override string ToString()
+    {
+        return point.ToString();// + cutDim.ToString();
+    }
+}
+
+class KDTree
+{
+    public KDNode root;
+    float maxVal;
+
+    // default constructor
+    //root is null and max float value of a point is 100
+    public KDTree()
+    {
+        this.root = null;
+        maxVal = 100.0f;
+    }
+
+    //overloaded constructor 1
+    //custom root and custom max point value
+    public KDTree(KDNode root, int max)
+    {
+        this.root = root;
+        maxVal = max;
+    }
+
+
+    //overloaded cuntstructor 2
+    //custom max point value
+    public KDTree(int max)
+    {
+        this.root = null;
+        maxVal = max;
+    }
+
+
+    // Insert method
+    //Inserts a point p into the KDTree
+    // takes a point p, a KDNode x to start at, and the cutting dimension cutDim to start at
+    public bool Insert(Point p,ref KDNode x, int cutDim)
+    {
+        for (int i = 0; i < p.getDim(); i++)   //Check if p's values are all less than the max value set for the tree
         {
-            this.root = root;
-            maxVal = max;
-        }
-
-
-        //overloaded cuntstructor 2
-        //custom max point value
-        public KDTree(int max)
-        {
-            this.root = null;
-            maxVal = max;
-        }
-
-
-        // Insert method
-        //Inserts a point p into the KDTree
-        // takes a point p, a KDNode x to start at, and the cutting dimension cutDim to start at
-        public bool Insert(Point p,ref KDNode x, int cutDim)
-        {
-            for (int i = 0; i < p.getDim(); i++)   //Check if p's values are all less than the max value set for the tree
-            {
-                if (p.Get(i) > maxVal)
-                    return false;
-            }
-
-            if (x == null)  //reached the bottom of the tree
-            {
-                x = new KDNode(p, cutDim); // create new leaf
-            }
-            else if (x.point.Equals(p)) //found a matching point in the tree
-            {
+            if (p.Get(i) > maxVal)
                 return false;
-            }
-            else if (x.inLeftSubtree(p))  // insert into left subtree
-            { 
-                return Insert(p, ref x.left, (x.cutDim + 1) % p.getDim());
-            }
-            else  // insert into right subtree
-            { 
-                return Insert(p, ref x.right, (x.cutDim + 1) % p.getDim());
-            }
+        }
 
-            return true; //if bottom of method reached point succsessfully inserted
+        if (x == null)  //reached the bottom of the tree
+        {
+            x = new KDNode(p, cutDim); // create new leaf
+        }
+        else if (x.point.Equals(p)) //found a matching point in the tree
+        {
+            return false;
+        }
+        else if (x.inLeftSubtree(p))  // insert into left subtree
+        { 
+            return Insert(p, ref x.left, (x.cutDim + 1) % p.getDim());
+        }
+        else  // insert into right subtree
+        { 
+            return Insert(p, ref x.right, (x.cutDim + 1) % p.getDim());
+        }
+
+        return true; //if bottom of method reached point succsessfully inserted
             
+    }
+
+        
+
+    //First Contains() method called by user
+    //takes a Point p and returns true if point is in the tree otherwise false
+    public bool Contains(Point p)   
+    {
+        return Contains(p, root);
+    }
+
+    //Second Contains() method called by first method^^^^
+    //takes the point from the first method and the root of the tree as arguments
+    //returns true if point in tree otherwise false
+    private bool Contains(Point p, KDNode root)
+    {
+        if (root.point.Equals(p))       //if point is in tree return true
+            return true;
+        else if (root.inLeftSubtree(p) && root.left != null )  //check left subtree if not null
+            return Contains(p, root.left);
+        else if(root.right != null)                 //check right subtree if not null
+            return Contains(p,root.right);  
+        else
+            return false;       //if nothing found return false
+    }
+
+
+    public KDNode delete(Point p)
+    {
+        if (this.Contains(p))
+            return delete(p, root);
+        else
+        {
+        Console.WriteLine("Point not found");
+            return null;
+        }
+    }
+    private KDNode delete(Point x, KDNode p)
+    {
+        if (p == null)
+        { // fell out of tree?
+            Console.WriteLine("point does not exist");
+            return null;
+        }
+        else if (p.point.Equals(x))
+        { // found it
+            if (p.right != null)
+            { // take replacement from right
+                p.point = findMin(p.right, p.cutDim);
+                p.right = delete(p.point, p.right);
+            }
+            else if (p.left != null)
+            { // take replacement from left
+                p.point = findMin(p.left, p.cutDim);
+                p.right = delete(p.point, p.left); // move left subtree to right!
+                p.left = null; // left subtree is now empty
+            }
+            else
+            { // deleted point in leaf
+                p = null; // remove this leaf
+            }
+        }
+        else if (p.inLeftSubtree(x))
+        {
+            p.left = delete(x, p.left); // delete from left subtree
+        }
+        else
+        { // delete from right subtree
+            p.right = delete(x, p.right);
+        }
+        return p;
+    }
+
+
+    private Point findMin(KDNode p, int i)// get min point along dim i
+    { 
+        if (p == null) // fell out of tree?
+        { 
+            return null;
+        }
+        if (p.cutDim == i)// cutting dimension matches i?
+        { 
+            if (p.left == null) // no left child?
+                return p.point; // use this point
+            else
+                return findMin(p.left, i); // get min from left subtree
+        }
+        else// it may be in either side
+        { 
+            Point q = minAlongDim(p.point, findMin(p.left, i), i);
+            return minAlongDim(q, findMin(p.right, i), i);
+        }
+    }
+
+    private Point minAlongDim(Point p1, Point p2, int i)
+    { // return smaller point on dim i
+        if (p2 == null || p1.Get(i) <= p2.Get(i)) // p1[i] is short for p1.get(i)
+            return p1;
+        else
+            return p2;
+    }
+
+
+
+
+
+    public void Print()     //not mine found on stack ofverflow
+    {
+        int depth = 0;
+        int order = 0;
+        Print(root, 1, depth);
+
+        List<string> levels = new List<string>();
+        Print2(root, depth, levels, ref order);
+        Console.WriteLine();
+        foreach (string level in levels)
+        {
+            Console.WriteLine(level);
+        }
+    }
+
+    public void Print(KDNode p, int padding, int depth)
+    {
+        if (p != null)
+        {
+            if (p.right != null)
+            {
+                Print(p.right, padding + 4, depth+1);
+            }
+            if (padding > 0)
+            {
+                Console.Write(" ".PadLeft(padding));
+            }
+            if (p.right != null)
+            {
+                Console.Write("/\n");
+                Console.Write(" ".PadLeft(padding));
+            }
+            Console.Write(depth + p.ToString() + "\n ");
+            if (p.left != null)
+            {
+                Console.Write(" ".PadLeft(padding) + "\\\n");
+                Print(p.left, padding + 4, depth + 1);
+            }
+        }
+    }
+
+    private void Print2(KDNode p, int depth, List<string> levels, ref int order)
+    {
+        while (levels.Count < (2* (depth + 2)))
+        {
+            levels.Add("");
+        }
+
+        if ((p.left != null)) 
+        {
+            Print2(p.left, depth + 1, levels, ref order);  
+        }
+
+
+        string s = levels.ElementAt(depth*2);
+        string s2 = levels.ElementAt((depth * 2)+1);
+
+        levels.RemoveAt(depth*2);
+        s = s + String.Concat(Enumerable.Repeat(" ", (order * (p.ToString()).Length) - s.Length)) + p.ToString();
+
+        levels.Insert(depth * 2, s);
+
+
+        levels.RemoveAt(depth * 2 +1);
+        s2 = s2 + String.Concat(Enumerable.Repeat(" ", (order * (p.ToString()).Length) - s2.Length)) ;
+
+        if (p.left != null)
+            s2 = s2 + "/";
+        if (p.right != null)
+            s2 = s2 + String.Concat(Enumerable.Repeat(" ", (p.ToString()).Length)) + "\\";
+
+        levels.Insert((depth * 2) + 1, s2);
+        
+
+        order++;
+
+        if ((p.right != null))
+        {
+            Print2(p.right, depth + 1, levels, ref order);
         }
 
         
 
-        //First Contains() method called by user
-        //takes a Point p and returns true if point is in the tree otherwise false
-        public bool Contains(Point p)   
-        {
-            return Contains(p, root);
-        }
-
-        //Second Contains() method called by first method^^^^
-        //takes the point from the first method and the root of the tree as arguments
-        //returns true if point in tree otherwise false
-        private bool Contains(Point p, KDNode root)
-        {
-            if (root.point.Equals(p))       //if point is in tree return true
-                return true;
-            else if (root.inLeftSubtree(p) && root.left != null )  //check left subtree if not null
-                return Contains(p, root.left);
-            else if(root.right != null)                 //check right subtree if not null
-                return Contains(p,root.right);  
-            else
-                return false;       //if nothing found return false
-        }
-
-
-        public KDNode delete(Point p)
-        {
-            if (this.Contains(p))
-                return delete(p, root);
-            else
-            {
-            Console.WriteLine("Point not found");
-                return null;
-            }
-        }
-        private KDNode delete(Point x, KDNode p)
-        {
-            if (p == null)
-            { // fell out of tree?
-                Console.WriteLine("point does not exist");
-                return null;
-            }
-            else if (p.point.Equals(x))
-            { // found it
-                if (p.right != null)
-                { // take replacement from right
-                    p.point = findMin(p.right, p.cutDim);
-                    p.right = delete(p.point, p.right);
-                }
-                else if (p.left != null)
-                { // take replacement from left
-                    p.point = findMin(p.left, p.cutDim);
-                    p.right = delete(p.point, p.left); // move left subtree to right!
-                    p.left = null; // left subtree is now empty
-                }
-                else
-                { // deleted point in leaf
-                    p = null; // remove this leaf
-                }
-            }
-            else if (p.inLeftSubtree(x))
-            {
-                p.left = delete(x, p.left); // delete from left subtree
-            }
-            else
-            { // delete from right subtree
-                p.right = delete(x, p.right);
-            }
-            return p;
-        }
-
-
-        private Point findMin(KDNode p, int i)// get min point along dim i
-        { 
-            if (p == null) // fell out of tree?
-            { 
-                return null;
-            }
-            if (p.cutDim == i)// cutting dimension matches i?
-            { 
-                if (p.left == null) // no left child?
-                    return p.point; // use this point
-                else
-                    return findMin(p.left, i); // get min from left subtree
-            }
-            else// it may be in either side
-            { 
-                Point q = minAlongDim(p.point, findMin(p.left, i), i);
-                return minAlongDim(q, findMin(p.right, i), i);
-            }
-        }
-
-        private Point minAlongDim(Point p1, Point p2, int i)
-        { // return smaller point on dim i
-            if (p2 == null || p1.Get(i) <= p2.Get(i)) // p1[i] is short for p1.get(i)
-                return p1;
-            else
-                return p2;
-        }
-
-
-
-
-
-        public void Print()     //not mine found on stack ofverflow
-        {
-            int depth = 0;
-            Print(root, 1, depth);
-        }
-
-        public void Print(KDNode p, int padding, int depth)
-        {
-            if (p != null)
-            {
-                if (p.right != null)
-                {
-                    Print(p.right, padding + 4, depth+1);
-                }
-                if (padding > 0)
-                {
-                    Console.Write(" ".PadLeft(padding));
-                }
-                if (p.right != null)
-                {
-                    Console.Write("/\n");
-                    Console.Write(" ".PadLeft(padding));
-                }
-                Console.Write(depth + p.ToString() + "\n ");
-                if (p.left != null)
-                {
-                    Console.Write(" ".PadLeft(padding) + "\\\n");
-                    Print(p.left, padding + 4, depth + 1);
-                }
-            }
-        }
     }
+}
+
+
 class Program
 {
     static void Main(string[] args)
